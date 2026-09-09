d144f4e2a19c2c31bbe9930cb07e2bb8a7dd2e2f
Taha Hadj Lakehal <140285251+TahaHL@users.noreply.github.com>
2026-09-09 18:05:05 +0100

    Update responsive foundation and Pages routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
 
+const app = <AuthProvider><App /></AuthProvider>
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
+    {import.meta.env.MODE === 'pages'
+      ? <HashRouter>{app}</HashRouter>
+      : <BrowserRouter>{app}</BrowserRouter>}
   </StrictMode>,
 )
